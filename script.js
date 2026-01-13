a3ca77180a99914995928d5e1e227a0408ed441b
Dharam Baghel <dharam892001@gmail.com>
2026-01-13 16:29:51 +0530

    again console added

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ console.log("This is version control system practice");
 console.log("This is Abhishek Kumar here");
 console.log("This is dharam here");     
 console.log("This is modification done by dharam");
+console.log("This is second modification done by dharam");
+console.log("This is third modification done by dharam");
