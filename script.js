a2ec96f75d033006b128365813cfdd5f422af6b9
Dharam Baghel <dharam892001@gmail.com>
2026-01-13 16:29:21 +0530

    console added

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("hello Branch 2 here");
+console.log("This is version control system practice");
+console.log("This is Abhishek Kumar here");
+console.log("This is dharam here");     
+console.log("This is modification done by dharam");
