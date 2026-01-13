b98d43b8c6b7e1d6f8cc3a581b69e8f55f94096e
Dharam Baghel <dharam892001@gmail.com>
2026-01-13 16:35:13 +0530

    more lines added in main branch

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log("hello Branch 2 here");
 console.log("this is dharam's main branch")
 console.log("this is  third line added in main branch")
-console.log("this is 4th line added in main branch")
+console.log("this is 4th line added in main branch")    
+console.log("this is 5th line added in main branch")
+console.log("this is 6th line added in main branch")
